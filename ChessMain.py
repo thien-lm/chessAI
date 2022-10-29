243c9616e771d7c51016c8f8c221b8f5c42ba421
thiencutovc <thien.lm204608@sis.hust.edu.vn>
2022-10-29 08:30:04 +0700

    con cac

---
+++ ChessMain.py
@@ -0,0 +1,101 @@
+import imp
+from tkinter import TRUE
+import pygame as p
+from chess_gem import ChessEngine
+import sys
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT//DIMENSION
+MAX_FPS = 24
+IMAGES = {}
+
+#bug after video 03 : white space can kill a chess element
+
+'''
+init a global dict of image, will call one
+'''
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load('images/' + piece +'.png'), (SQ_SIZE, SQ_SIZE))
+   #acess image by images[piece]
+
+def drawGameState(screen, gs):
+    drawBoard(screen)#dRaw square
+    #addin suggestion
+    drawPieces(screen, gs.board)#draw pieces
+
+def drawBoard(screen):
+    colors = [p.Color('white'), p.Color('gray')]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[ ( (r+c) % 2 ) ]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r* SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawPieces(screen, board):       
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r* SQ_SIZE, SQ_SIZE, SQ_SIZE)) 
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color('white'))
+    gs = ChessEngine.GameState()
+
+    validMoves = gs.getValidMoves()
+    moveMade = False #flag when a move is made
+    #print(gs.board) # this is a board representation
+    loadImages()
+    running = True
+
+    sqSelected = () #no quare selected, keep track last  click
+    playerClicks = [] # keep track player click
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                sys.exit()
+            #mouse handler    
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()#mouse location(x,y)
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                if sqSelected == (row, col):
+                    sqSelected = () #click same square == undo action 
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2: #after second click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board) 
+                    print(move.getChessNotation())
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
+                    gs.makeMove(move)
+                    sqSelected = () #reset user click
+                    playerClicks = [] 
+
+            #key Handler
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:#press z to undo
+                    gs.undoMove()
+                    print('undo')
+                    moveMade = True
+                    validMoves = gs.getValidMoves()
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
+
+
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()     
+
+
+main()    
---
+++ chess_gem/ChessEngine.py
@@ -0,0 +1,103 @@
+#TODO: use numpy instead of array
+
+from multiprocessing import RLock
+from chess import Board
+from numpy import blackman
+
+
+class GameState():
+    def __init__(self):
+        # represent a 8x8 chess Board
+        # bR = black Rock
+        # wR = white Rock
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove =  True
+        self.moveLog = [] 
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move) #log the move to display it later
+        self.whiteToMove = not self.whiteToMove #switch turn
+
+    ''' undo the lastMove'''  
+    def undoMove(self):
+        if len(self.moveLog) != 0:
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove
+    '''
+    checking move
+    '''        
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
+
+    '''
+    all moves without check
+    '''
+
+    def getAllPossibleMoves(self):
+        moves = [Move((6, 4), (4, 4), self.board)]
+
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if (turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                    piece = self.board[r][c][1]
+                    if piece == 'p':
+                        self.getPawnMoves(r, c, moves)
+                    elif piece == 'R':
+                        self.getRookMoves(r, c, moves)    
+
+        return moves                
+
+    '''get all pawn move'''
+
+    def getPawnMoves(self, r, c, moves):
+        pass
+
+    def getRookMoves(self, r, c, moves):
+        pass
+
+
+
+
+class Move():
+    #map keys to values
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow*1000 + self.startCol*100 + self.endRow*10 + self.endCol
+        print(self.moveID)
+
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]        
